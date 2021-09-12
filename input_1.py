540b3207dc07ac4ff677e872afcbccf14a95d975
jamie082 <jamiemorrissey02@gmail.com>
2021-09-12 09:06:32 +0000

    added source file

---
+++ input_1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+num = float(input("Enter a number: "))
+if num >= 50:
+    if num == 50:
+        print("Fifty")
+    else:
+        print("Greater than Fifty")
+else:
+    print("Less than Fifty")
